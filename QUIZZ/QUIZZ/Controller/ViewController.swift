d21e8b43cfdf476176e96401c88a7ddd4c1d7af9
MadNoodle <mjanneau@gmail.com>
2017-11-13 12:04:30 +0100

    Ajout des icones, images et du launchScreen

---
+++ QUIZZ/QUIZZ/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QUIZZ
+//
+//  Created by Mathieu Janneau on 13/11/2017.
+//  Copyright © 2017 Mathieu Janneau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
