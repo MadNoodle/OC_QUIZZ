78d0c9450e43bf74a98d8b2c118a0c3cdb9585c2
MadNoodle <mjanneau@gmail.com>
2017-11-13 14:13:36 +0100

    mise en place des models Questions, Game et QuestionMAnager

---
+++ QUIZZ/QUIZZ/Model/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  QUIZZ
+//
+//  Created by Mathieu Janneau on 13/11/2017.
+//  Copyright © 2017 Mathieu Janneau. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+  var title = ""
+  var isCorrect = false
+  
+  
+}
